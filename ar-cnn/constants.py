7f387e9f818c1ab89e69238e20bfe1df2d6ded97
Alex <asigalov61@gmail.com>
2020-08-22 07:09:11 -0700

    Update constants.py

--- ar-cnn/constants.py
+++ ar-cnn/constants.py
@@ -24,15 +24,15 @@ class Constants():
     samples_per_ground_truth_data_item = 8
     training_validation_split = 0.9
     # Number of Bars
-    bars = 8
+    bars = 64
     # Number of Beats Per Bar
     beats_per_bar = 4
     beat_resolution = 4
     # number of bars to be shifted
     bars_shifted_per_sample = 4
     # Total number of pitches in a Pianoroll
-    number_of_pitches = 128
+    number_of_pitches = 1024
     # Total number of Tracks
     number_of_channels = 1
     output_file_path = "outputs/output_{}.mid"
-    tempo = 100  # 100 bpm
+    tempo = 120  # 100 bpm
